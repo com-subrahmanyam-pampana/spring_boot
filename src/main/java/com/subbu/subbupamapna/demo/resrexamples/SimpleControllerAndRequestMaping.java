c1948e54ed6c3b3f6117d18a11c74f1f6e843084
Subbu <subrahmanyam@jungleegames.com>
2022-07-12 12:25:57 +0530

    rest example added

---
+++ src/main/java/com/subbu/subbupamapna/demo/resrexamples/SimpleControllerAndRequestMaping.java
@@ -0,0 +1,18 @@
+package com.subbu.subbupamapna.demo.resrexamples;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping()
+public class SimpleControllerAndRequestMaping {
+	
+	@GetMapping(value="/",produces = "application/json")
+	    public @ResponseBody String getBook() {
+	        return "Hello World!!!";
+	    } 
+}
+
+
